e9f4322b07088b31b7b82d7b43ec4719b769b2da
José Andrés <joseacevedofallas@gmail.com>
2026-06-08 23:48:33 -0600

    Agrega BookDTO para transferencia de datos entre capas

---
+++ DTOs/BookDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Gestor_Libros_EF.DTOs
+{
+    public class BookDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public int YearPublished { get; set; }
+    }
+}
