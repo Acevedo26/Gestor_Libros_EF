0ae40964ced234fdb5680157bfcf90fb614f81f1
José Andrés <joseacevedofallas@gmail.com>
2026-06-08 23:48:38 -0600

    Agrega IBookRepository e implementacion BookRepository con operaciones CRUD

---
+++ Repositories/BookRepository.cs
@@ -0,0 +1,64 @@
+﻿using Gestor_Libros_EF.Data;
+using Gestor_Libros_EF.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gestor_Libros_EF.Repositories
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly AppDbContext _context;
+
+        // Constructor de inyección de dependencias
+        public BookRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // Implementación de métodos CRUD
+
+        // Agrega un nuevo libro a la base de datos
+        public async Task AddAsync(Book book)
+        {
+             await _context.Books.AddAsync(book);
+             await _context.SaveChangesAsync();    
+        }
+
+        // Obtiene todos los libros de la base de datos
+        public async Task<IEnumerable<Book>> GetAllAsync()
+        {
+            return await _context.Books.ToListAsync();
+        }
+
+        // Obtiene un libro por su ID
+        public async Task<Book> GetByIdAsync(int id)
+        {
+            return await _context.Books.FindAsync(id);
+        }
+
+        // Obtiene libros por título
+        public async Task<IEnumerable<Book>> GetByTitleAsync(string title)
+        {
+            return await _context.Books
+                .Where(b => b.Title.Contains(title))
+                .ToListAsync();  
+        }
+
+        // Actualiza un libro existente en la base de datos
+        public async Task UpdateAsync(Book book)
+        {
+            _context.Books.Update(book);
+            await _context.SaveChangesAsync();
+        }
+
+        // Elimina un libro de la base de datos por su ID
+        public async Task DeleteAsync(int id)
+        {
+            var book = await _context.Books.FindAsync(id);
+            if (book != null)
+            {
+                _context.Books.Remove(book);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ Repositories/IBookRepository.cs
@@ -0,0 +1,16 @@
+﻿
+using Gestor_Libros_EF.Models;
+
+namespace Gestor_Libros_EF.Repositories
+{
+    public interface IBookRepository
+    {
+        Task AddAsync(Book book);
+        Task<IEnumerable<Book>> GetAllAsync(); 
+        Task<Book> GetByIdAsync(int id);
+        Task<IEnumerable<Book>> GetByTitleAsync(string title);
+        Task UpdateAsync(Book book);
+        Task DeleteAsync(int id);
+
+    }
+}
